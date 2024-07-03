aa5b47ea28bb4d01700fa50bd3824dcce67b5204
xuanzhi33 <xuanzhi33@qq.com>
2024-07-03 19:21:40 +0800

    fix: CORS

--- server/main.py
+++ server/main.py
@@ -5,25 +5,12 @@ store = Store()
 
 app = Flask(__name__)
 
-allow_origins = [
-    "https://www.xuanzhi33.cn",
-    "https://xuanzhi33.cn",
-    "https://gh.xuanzhi33.cn",
-    "http://localhost:5500"
-]
 
-def cors_response(data):
-    origin = request.headers.get('Origin')
-    if origin in allow_origins:
-        response = make_response(jsonify(data))
-        response.headers['Access-Control-Allow-Origin'] = origin
-        return response
-    else:
-        return jsonify({
-            'code': 418,
-            'msg': 'I\'m a teapot.'
-        }), 418
-    
+def cors_response(data):    
+    response = make_response(jsonify(data))
+    response.headers['Access-Control-Allow-Origin'] = "*"
+    return response
+
 
 @app.route('/api/insert', methods=['POST'])
 def insert():
