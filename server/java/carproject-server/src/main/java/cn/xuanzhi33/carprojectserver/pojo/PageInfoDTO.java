e3b37b472331a68e67626e369ebbf7d9966d7af7
xuanzhi33 <xuanzhi33@qq.com>
2024-07-15 20:41:27 +0800

    feat: pageSize validation

--- server/java/carproject-server/src/main/java/cn/xuanzhi33/carprojectserver/pojo/PageInfoDTO.java
+++ server/java/carproject-server/src/main/java/cn/xuanzhi33/carprojectserver/pojo/PageInfoDTO.java
@@ -1,13 +1,13 @@
 package cn.xuanzhi33.carprojectserver.pojo;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 public class PageInfoDTO {
     @Min(value = 1, message = "page should be greater than 0")
     private int page;
-    @Max(value = 100, message = "pageSize should be less than 100")
+    @Range(min = 1, max = 100, message = "pageSize should be between 1 and 100")
     private int pageSize;
 }
