79b8122df6196c8568c184624bc23ff8102d463f
xuanzhi33 <xuanzhi33@qq.com>
2024-07-20 11:42:03 +0800

    chore: Update DB

--- server/java/carproject-server/init_database.sql
+++ server/java/carproject-server/init_database.sql
@@ -2,9 +2,7 @@ create table sensor_data
 (
     id          int auto_increment
         primary key,
-    user       varchar(50)                        not null,
-    data        text                               null,
+    user        varchar(50)                        not null,
+    data        mediumtext                         null,
     create_time datetime default CURRENT_TIMESTAMP not null
 );
-
-
